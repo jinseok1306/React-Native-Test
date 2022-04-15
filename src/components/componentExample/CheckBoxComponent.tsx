537691e4bec4d3145f12bcd18a524ebadae403dd
jinseok.park <jinseok1306@seetec.com>
2022-04-15 09:27:35 +0900

    checkbox 컴포넌트 가운데 정렬 수정

--- src/components/componentExample/CheckBoxComponent.tsx
+++ src/components/componentExample/CheckBoxComponent.tsx
@@ -11,54 +11,56 @@ const CheckBoxComponent = () => {
     return (
         <View style={styles.container}>
             <StatusBar barStyle="dark-content" />                                             
-            <CheckBox
-                center
-                title="Click Here"
-                checked={check1}
-                onPress={() => setCheck1(!check1)}
-            />
-            <CheckBox
-                center
-                title="Click Here"
-                checkedIcon="dot-circle-o"
-                uncheckedIcon="circle-o"
-                checked={check2}
-                onPress={() => setCheck2(!check2)}
-            />
-            <CheckBox
-                center
-                title={`Click Here to ${check3 ? 'Remove' : 'Add'} This Item`}
-                iconRight
-                iconType="material"
-                checkedIcon="clear"
-                uncheckedIcon="add"
-                checkedColor="red"
-                checked={check3}
-                onPress={() => setCheck3(!check3)}
-            />
-            <CheckBox
-                center
-                checkedIcon={
-                    <Icon
-                    name="radio-button-checked"
-                    type="material"
-                    color="green"
-                    size={25}
-                    iconStyle={{ marginRight: 10 }}
-                    />
-                }
-                uncheckedIcon={
-                    <Icon
-                    name="radio-button-unchecked"
-                    type="material"
-                    color="grey"
-                    size={25}
-                    iconStyle={{ marginRight: 10 }}
-                    />
-                }
-                checked={check4}
-                onPress={() => setCheck4(!check4)}
-            />
+            <View style={styles.contentView}>
+                <CheckBox
+                    center
+                    title="Click Here"
+                    checked={check1}
+                    onPress={() => setCheck1(!check1)}
+                />
+                <CheckBox
+                    center
+                    title="Click Here"
+                    checkedIcon="dot-circle-o"
+                    uncheckedIcon="circle-o"
+                    checked={check2}
+                    onPress={() => setCheck2(!check2)}
+                />
+                <CheckBox
+                    center
+                    title={`Click Here to ${check3 ? 'Remove' : 'Add'} This Item`}
+                    iconRight
+                    iconType="material"
+                    checkedIcon="clear"
+                    uncheckedIcon="add"
+                    checkedColor="red"
+                    checked={check3}
+                    onPress={() => setCheck3(!check3)}
+                />
+                <CheckBox
+                    center
+                    checkedIcon={
+                        <Icon
+                        name="radio-button-checked"
+                        type="material"
+                        color="green"
+                        size={25}
+                        iconStyle={{ marginRight: 10 }}
+                        />
+                    }
+                    uncheckedIcon={
+                        <Icon
+                        name="radio-button-unchecked"
+                        type="material"
+                        color="grey"
+                        size={25}
+                        iconStyle={{ marginRight: 10 }}
+                        />
+                    }
+                    checked={check4}
+                    onPress={() => setCheck4(!check4)}
+                />
+            </View>            
         </View>
     );
 }
@@ -68,6 +70,9 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: 'white',
         justifyContent: 'center',
+    },
+    contentView: {
+        flex: 1,
     },
     button: {
         margin: 10,
