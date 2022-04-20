562f3b20e0add196f7b109cd4e3385669967495d
jinseok.park <jinseok1306@seetec.com>
2022-04-20 11:26:30 +0900

    SQLite DB 연동 및 쿼리문 테스트 완료

--- src/components/SQLiteScreen.tsx
+++ src/components/SQLiteScreen.tsx
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet, StatusBar } from 'react-native';
+import SQLite from 'react-native-sqlite-storage';
+
+let db: SQLite.SQLiteDatabase;
 
 const SQLiteScreen = () => {
+    useEffect(() => {
+        db = SQLite.openDatabase(
+        {
+            name: 'TestDB',
+            location: 'default',
+            createFromLocation: 1,
+        },
+        () => {
+            console.log('불러오기 성공');
+
+            db.transaction(tx => {                
+                tx.executeSql(`select * from test`, [], (tx, results) => {                                  
+                  const rows = results.rows;
+                  let users = [];
+          
+                  for (let i = 0; i < rows.length; i++) {
+                    users.push({
+                      ...rows.item(i),
+                    });
+                  }    
+                  console.log(users);
+                });
+              });
+        },
+        (error) => {
+            console.log('에러발생: ', error);
+        });
+    }, [])
+
     return (
         <View style={styles.container}>
             <StatusBar barStyle="dark-content" />
